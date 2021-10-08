5e814f2d8b94e37489b54d8298f775c0af42d0b3
Roman Strypa <gfdg@bbbb.yyy>
2021-10-08 19:28:38 +0300

    0.2v it works!

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const TelegramApi = require('node-telegram-bot-api');
 const {keyBoard} = require('./options');
 const sequelize = require('./db');
 
-
-const TOKEN = 'token'
-//import {TOKEN} from './config.js'
+const TOKEN = '1080884640:AAGEU_eNkThOcc0AzlGOpUaMbD_m2O7z-xs'
 
 const bot = new TelegramApi(TOKEN, {polling: true})
 
@@ -14,28 +12,22 @@ const start = async () => {
         await sequelize.authenticate()
         await sequelize.sync()
     } catch (e) {
-        console.log('The connection is broken...', e)
+        console.log('The connection with database is broken...', e)
     }
-
-/*    connection.query(sql, function(err, results) {
-        if(err) console.log(err);
-        console.log(results);
-    });
-
-    connection.end();*/
 
     bot.setMyCommands( [
         {command: '/start', description: 'Початкове вітання'},
         {command: '/search', description: 'Перейти до пошуку'},
         {command: '/about', description: 'інформація про мене, або для чого мене створили'},
 //    {command: '/info', description: ''},
-        {command: '/clear', description: 'Очистити'},
+        {command: '/help', description: 'Очистити'},
     ])
 
-    bot.on('message',msg => {
+    bot.on("polling_error", (err) => console.log(err));
+
+    bot.on('message', async msg => {
         const text = msg.text;
         const chatId = msg.chat.id;
-//    bot.sendMessage(chatId, `Ти написав мені ${text}`)
         console.log(msg)
 
         try {
@@ -43,19 +35,24 @@ const start = async () => {
                 return bot.sendMessage(chatId, `Дякую, що ти обрав саме мене. Я спробую знайти твою книжку. Але перш ніж перейти до пошуку, пропоную тобі ознайомитися зі мною та моїми можливостями))`, keyBoard);
             }
             if (text === '/search') {
-              //  const BookController = require('./controller/BookController')
-                return bot.sendMessage(chatId, 'in the process of development... please wait /about')
+                bot.sendMessage(chatId, "Введи назву чи автора. Якщо не пам'ятаєш, напиши приблизно, а я спробую знайти!");
+                bot.on('message', (ctx) => {
+                    const text = ctx.text
+                    const log = sequelize.query("SELECT book FROM books t WHERE (t.*)::text LIKE '%"+ text +"%'") .then( (result) => {
+                        console.log(result,log);
+                    }) .catch( (err)  => {
+                        console.log(err);
+                    })
+                })
             }
             if (text === '/about') {
-                return bot.sendMessage(chatId, `Ідея створити мене була в 2020 році. Моїм засновником і творцем є Стрипа Роман (https://www.linkedin.com/in/roman-strypa-b4623a205/). Він був студентом ЛНУ та системним адміністратором наукової бібліотеки. Моїм завданням є простий пошук книг. Тобто ти пишеш назву книжки або ж якесь інше значення, наприклад автора, місце видання тощо, і я повертаю тобі підібрані книжки. Цей проект є волонтерським та орієнтований на студентів, а також всіх, хто користується Телеграмом і відвідує бібліотеку ЛНУ Франка. Сподіваюсь мені вдасться допомогти тобі з пошуком літератури./search`);
+                return bot.sendMessage(chatId, `Ідея створити мене була в 2020 році. Моїм засновником і творцем є Стрипа Роман (https://www.linkedin.com/in/roman-strypa-b4623a205/). Він був студентом ЛНУ та системним адміністратором наукової бібліотеки. Моїм завданням є простий пошук книг. Тобто ти пишеш назву книжки або ж якесь інше значення, наприклад автора, місце видання тощо, і я повертаю тобі підібрані книжки. Цей проєкт є волонтерським та орієнтований на студентів, а також всіх, хто користується Телеграмом і відвідує бібліотеку ЛНУ Франка. Сподіваюсь мені вдасться допомогти тобі з пошуком літератури./search`);
             }
-            if (text === '/clear') {
-                //   return bot.deleteMessage(chatId, messages.delete())
-            } else {
-                bot.sendMessage(chatId, 'Я тебе не розумію./search')
+            if (text === '/help') {
+                //return bot.sendMessage(chatId, '')
             }
         } catch (e) {
-            return bot.sendMessage(chatId, 'ERROR')
+            return bot.sendMessage(chatId, 'ERROR');
         }
     })
     bot.on('callback_query', msg => {
