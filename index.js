9ef49cf403fb24bd35120590817852abbec0ce2a
Roman Strypa <gfdg@bbbb.yyy>
2021-10-09 14:12:19 +0300

    0.2version

--- index.js
+++ index.js
@@ -36,14 +36,101 @@ const start = async () => {
             }
             if (text === '/search') {
                 bot.sendMessage(chatId, "Введи назву чи автора. Якщо не пам'ятаєш, напиши приблизно, а я спробую знайти!");
-                bot.on('message', (ctx) => {
+
+
+
+
+
+                bot.on('message', async (message) => {
+                    const {text, chat} = message; // https://core.telegram.org/bots/api#message
+                    const {id: chatId} = chat; // https://core.telegram.org/bots/api#chat
+
+                    let response = '';
+
+                    try {
+                        const [rows, metadata] = await sequelize.query(`SELECT book FROM books t WHERE (t.*)::text LIKE '%${text}%'`);
+                        if (rows.length) {
+                            response = rows.map(row => row.book).join("\n");
+                        }
+                        else {
+                            response = 'Book not found';
+                        }
+                    }
+                    catch (error) {
+                        console.error(error.message);
+                        response = 'Unable to lookup';
+                    }
+                    finally {
+                        if (response) {
+                            bot.sendMessage(chatId, response);
+                        }
+                    }
+                })
+
+
+
+
+
+
+                /*bot.on('message', async (message) => {
+                    const {text, chat} = message; // https://core.telegram.org/bots/api#message
+                    const {id: chatId} = chat; // https://core.telegram.org/bots/api#chat
+
+                    let response = '';
+
+                    try {
+                        const rows = await sequelize.query(`SELECT book FROM books t WHERE (t.*)::text LIKE '%${text}%'`);
+                        if (rows.length) {
+                            response = rows.map(row => row.book).join("\n");
+                        }
+                        else {
+                            response = 'Book not found';
+                        }
+                    }
+                    catch (error) {
+                        console.error(error.message);
+                        response = 'Unable to lookup';
+                    }
+                    finally {
+                        if (response) {
+                            bot.sendMessage(chatId, response);
+                        }
+                    }
+                })*/
+
+
+
+
+
+                /*bot.on('message', async (ctx) => {
+                    try {
+                        const {text, chat: {id: chatId}} = ctx;
+                        const rows = await sequelize.query("SELECT book FROM books t WHERE (t.*)::text LIKE '%"+ text +"%'");
+                        const response = rows.map(row => row.book).join("\n");
+
+                        bot.sendMessage(chatId, response);
+                    }
+                    catch (error) {
+                        console.error(error.message);
+                        ctx.reply('Unable to lookup');
+                    }
+                })*/
+
+
+
+
+
+                /*bot.on('message', (ctx) => {
                     const text = ctx.text
                     const log = sequelize.query("SELECT book FROM books t WHERE (t.*)::text LIKE '%"+ text +"%'") .then( (result) => {
-                        console.log(result,log);
+                        console.log(result,log)
                     }) .catch( (err)  => {
                         console.log(err);
+                        for (const result of results) {
+                            ctx.reply(result.book);
+                        }
                     })
-                })
+                })*/
             }
             if (text === '/about') {
                 return bot.sendMessage(chatId, `Ідея створити мене була в 2020 році. Моїм засновником і творцем є Стрипа Роман (https://www.linkedin.com/in/roman-strypa-b4623a205/). Він був студентом ЛНУ та системним адміністратором наукової бібліотеки. Моїм завданням є простий пошук книг. Тобто ти пишеш назву книжки або ж якесь інше значення, наприклад автора, місце видання тощо, і я повертаю тобі підібрані книжки. Цей проєкт є волонтерським та орієнтований на студентів, а також всіх, хто користується Телеграмом і відвідує бібліотеку ЛНУ Франка. Сподіваюсь мені вдасться допомогти тобі з пошуком літератури./search`);
