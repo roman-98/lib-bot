8a0d9a0564e12e56f1b4dec623fb0ae8a3e93eda
Roman Strypa <gfdg@bbbb.yyy>
2021-10-11 14:46:05 +0300

    0.3

--- index.js
+++ index.js
@@ -1,75 +1,53 @@
 const TelegramApi = require('node-telegram-bot-api');
-const {keyBoard} = require('./options');
 const sequelize = require('./db');
 
-const TOKEN = 'token'
+const TOKEN = require('./config');
 
-const bot = new TelegramApi(TOKEN, {polling: true})
+const bot = new TelegramApi(TOKEN, {polling: true});
+
+bot.on('message', async msg => {
+    console.log(msg)
+    })
 
 const start = async () => {
 
     try {
-        await sequelize.authenticate()
-        await sequelize.sync()
+        await sequelize.authenticate();
+        await sequelize.sync();
     } catch (e) {
-        console.log('The connection with database is broken...', e)
+        console.log('The connection with database is broken...', e);
     }
-
-    bot.setMyCommands( [
-        {command: '/search', description: 'Перейти до пошуку'},
-    ])
 
     bot.on("polling_error", (err) => console.log(err));
 
-    bot.on('message', async msg => {
-        const text = msg.text;
-        const chatId = msg.chat.id;
-        console.log(msg)
+        bot.on('message', async (message) => {
+            const {text, chat} = message;
+            const {id: chatId} = chat;
 
-        try {
-            if (text === '/start') {
-                return bot.sendMessage(chatId, `Дякую, що ти обрав саме мене. Я спробую знайти твою книжку. Але перш ніж перейти до пошуку, пропоную тобі ознайомитися зі мною та моїми можливостями))`, keyBoard);
+            let response = '';
+
+            try {
+                const [rows] = await sequelize.query(`SELECT book FROM books t WHERE (t.*)::text LIKE '%${text}%'`);
+                if (rows.length) {
+                    response = rows.map(row => row.book).join("\n");
+                } else {
+                    response = 'Введіть автора, назву книги чи будь-якого іншого видання. Якщо я не знайду потрібної вам літератури, спробуйте конкретизувати запит або підіть у бібліотеку (м.Львів, вул.Драгоманова 5(17)). Можливо потрібна вам література є в іншому каталозі, якого я не знаю.';
+                }
+            } catch (error) {
+                console.error(error.message);
+                response = 'Не вдається знайти';
+            } finally {
+                if (response) {
+                    bot.sendMessage(chatId, response);
+                }
             }
-            if (text === '/search') {
-                bot.sendMessage(chatId, "Введи назву чи автора. Якщо не пам'ятаєш, напиши приблизно, а я спробую знайти!");
-
-                bot.on('message', async (message) => {
-                    const {text, chat} = message; // https://core.telegram.org/bots/api#message
-                    const {id: chatId} = chat; // https://core.telegram.org/bots/api#chat
-
-                    let response = '';
-
-                    try {
-                        const [rows, metadata] = await sequelize.query(`SELECT book FROM books t WHERE (t.*)::text LIKE '%${text}%'`);
-                        if (rows.length) {
-                            response = rows.map(row => row.book).join("\n");
-                        }
-                        else {
-                            response = 'Book not found';
-                        }
-                   }
-                    catch (error) {
-                        console.error(error.message);
-                        response = 'Unable to lookup';
-                    }
-                    finally {
-                        if (response) {
-                            bot.sendMessage(chatId, response);
-                        }
-                    }
-                })
-            }
-        } catch (e) {
-            return bot.sendMessage(chatId, 'ERROR');
-        }
-    })
+        })
     bot.on('callback_query', msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
-        bot.sendMessage(chatId, `${data}`)
-        console.log(msg)
+        bot.sendMessage(chatId, `${data}`);
+        console.log(msg);
     })
-
 }
 
-start()
+start();
